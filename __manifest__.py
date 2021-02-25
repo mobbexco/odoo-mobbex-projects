b5beb0943176d60766e0d3507bc621970157e7a0
alexisvillagra91 <alexis.villagra@mobbex.com>
2021-02-25 00:44:32 -0300

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Mobbex Checkout',
+    'version': '1.0.0',
+    'author': 'Mobbex',
+    'website': 'https://www.mobbex.com/',
+    'category': 'Accounting/Payment',
+    'summary': 'A module that provides Odoo Mobbex integration.',
+    'description': """The Mobbex Payment Gateway redirects customers to Mobbex to enter their payment information.""",
+    'depends': ['payment'],
+    'installable': True,
+}
---
+++ models/models.py
@@ -0,0 +1,3 @@
+from odoo import models, fields
+
+class Checkout(models.Model):
